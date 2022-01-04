588ff287eb602849975cc6650c622d1f7455ffa9
Piotr <p.pozarowszczyk@outlook.com>
2022-01-04 13:39:21 +0100

    Before adding NuGet

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,22 +1,9 @@
 ﻿using MotoApp;
+using MotoApp.Entities;
+using MotoApp.Repositories;
 
-var stack = new Stack<double>();
-var stackString = new Stack<String>();
-stack.Push(4.5);
-stack.Push(43);
-stack.Push(333.6);
-
-stackString.Push("Company");
-stackString.Push("Company");
-stackString.Push("Company");
-
-double sum = 0.0;
-
-while(stack.Count > 0)
-{
-    double item = stack.Pop();
-    Console.WriteLine($"item: {item}");
-    sum += item;
-}
-
-Console.WriteLine(sum);
+var employeeRepository = new GenericRepository<Employee, Guid>();
+employeeRepository.Add(new Employee { FirstName = "Adam" });
+employeeRepository.Add(new Employee { FirstName = "Piotr" });
+employeeRepository.Add(new Employee { FirstName = "Zuzia" });
+employeeRepository.Save();
---
+++ MotoApp/Repositories/GenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using MotoApp.Entities;
+
+namespace MotoApp.Repositories
+{
+    public class GenericRepository<T> 
+        where T : class, IEntity, new()
+    {
+        protected readonly List<T> _items = new();
+
+        public T GetById(int id)
+        {
+            return _items.Single(item => item.Id == id);
+        }
+
+        public void Add(T item)
+        {
+            item.Id = _items.Count + 1;
+            _items.Add(item);
+        }
+
+        public void Save()
+        {
+            foreach(var item in _items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
