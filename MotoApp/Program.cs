47c52fd5f29326d02d7afc8ed81593698db19f34
Piotr <p.pozarowszczyk@outlook.com>
2022-03-15 12:51:54 +0100

    Read from files

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using MotoApp;
+using MotoApp.Components.CsvReader;
 using MotoApp.Components.DataProviders;
 using MotoApp.Data.Entities;
 using MotoApp.Data.Repositories;
@@ -9,6 +10,7 @@ services.AddSingleton<IApp, App>();
 services.AddSingleton<IRepository<Employee>, ListRepository<Employee>>();
 services.AddSingleton<IRepository<Car>, ListRepository<Car>>();
 services.AddSingleton<ICarsProvider, CarsProvider>();
+services.AddSingleton<ICsvReader, CsvReader>();
 
 var serviceProvider = services.BuildServiceProvider();
 var app = serviceProvider.GetService<IApp>()!;
