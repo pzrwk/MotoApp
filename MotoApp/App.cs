8212dce7d65671c52b025b165f40417a9a72bd28
Piotr <p.pozarowszczyk@outlook.com>
2022-03-15 16:35:03 +0100

    Use GroupJoin method

--- MotoApp/App.cs
+++ MotoApp/App.cs
@@ -17,41 +17,25 @@ public class App : IApp
 
         var manufacturers = _csvReader.ProcessManufacturers("Resources\\Files\\manufacturers.csv");
 
-        /*var groups = cars
-            .GroupBy(x => x.Manufacturer)
-            .Select(g => new
+        var groups = manufacturers.GroupJoin(
+            cars,
+            manufacturer => manufacturer.Name,
+            car => car.Manufacturer,
+            (m, g) => new
             {
-                Name = g.Key,
-                Max = g.Max(c => c.Combined),
-                Avg = g.Average(c => c.Combined)
+                Manufacturer = m,
+                Cars = g
             })
-            .OrderBy(x => x.Avg);
+            .OrderBy(x => x.Manufacturer.Name);
 
-        foreach (var group in groups)
+        foreach (var car in groups)
         {
-            Console.WriteLine($"{group.Name}");
-            Console.WriteLine($"\tMax: {group.Max}");
-            Console.WriteLine($"\tAverage: {group.Avg}");
-        }*/
-
-        var carsInCountry = cars.Join(
-                    manufacturers,
-                    c => new { c.Manufacturer, c.Year },
-                    m => new { Manufacturer = m.Name, m.Year},
-                    (car, manufacturer) => new
-                    {
-                        manufacturer.Country,
-                        car.Name,
-                        car.Combined
-                    })
-                .OrderByDescending(x => x.Combined)
-                .ThenBy(x => x.Name);
-
-        foreach (var car in carsInCountry)
-        {
-            Console.WriteLine($"Country: {car.Country}");
-            Console.WriteLine($"\tName: {car.Name}");
-            Console.WriteLine($"\tCombined: {car.Combined}");
+            Console.WriteLine($"Manufacturer: {car.Manufacturer.Name}");
+            Console.WriteLine($"\t Cars: {car.Cars.Count()}");
+            Console.WriteLine($"\t Max {car.Cars.Max(x => x.Combined)}");
+            Console.WriteLine($"\t Min {car.Cars.Min(x => x.Combined)}");
+            Console.WriteLine($"\t Avg {car.Cars.Average(x => x.Combined)}");
+            Console.WriteLine();
         }
     }
 }
