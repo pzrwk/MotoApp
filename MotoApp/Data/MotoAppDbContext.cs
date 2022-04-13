8b4f648cb5de408c4586a1a3fef30872c6e25933
pzrwk <p.pozarowszczyk@outlook.com>
2022-04-13 22:14:57 +0200

    Add EntityFramework & EntityFrameworkSQLServer

--- MotoApp/Data/MotoAppDbContext.cs
+++ MotoApp/Data/MotoAppDbContext.cs
@@ -5,14 +5,14 @@ namespace MotoApp.Data
 {
     public class MotoAppDbContext : DbContext
     {
-        public DbSet<Employee> Employees => Set<Employee>();
+        //public DbSet<Employee> Employees => Set<Employee>();
 
-        public DbSet<BusinessPartner> BusinessPartners => Set<BusinessPartner>();
+        //public DbSet<BusinessPartner> BusinessPartners => Set<BusinessPartner>();
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseInMemoryDatabase("StorageAppDb");
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+        //    optionsBuilder.UseInMemoryDatabase("StorageAppDb");
+        //}
     }
 }
